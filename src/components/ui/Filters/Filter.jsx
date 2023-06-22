c4593c764e86d24a756350d97b70561be7a3291d
Rubcs <Rubcs@gmail.com>
2023-06-22 11:48:49 +0200

    try reduce method

--- src/components/ui/Filters/Filter.jsx
+++ src/components/ui/Filters/Filter.jsx
@@ -2,16 +2,49 @@ import React, { useState, useEffect, useContext } from 'react';
 import './Filter.css';
 import { ProductContext } from '../../../App';
 
-const Filter = ({ inputTitle, inputOptions, categoriesData }) => {
+const Filter = ({ inputTitle, inputOptions, currentPath, categoriesData }) => {
   const { productsToRender, setProductsToRender } = useContext(ProductContext);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [allFilteredProducts, setAllFilteredProducts] = useState([]);
+
+  // const handleCheckbox = (ev) => {
+  //   const filteredProducts = productsToRender.filter(
+  //     (product) => product.name.split(' ')[0] === ev.target.value
+  //   );
+  //   setProductsToRender(filteredProducts);
+  // };
+
+  // useEffect(() => {
+  //   setProductsToRender(allFilteredProducts);
+  // }, [allFilteredProducts]);
 
   const handleCheckbox = (ev) => {
-    const filteredProducts = productsToRender.filter(
-      (product) => product.name.split(' ')[0] === ev.target.value
-    );
-    setProductsToRender(filteredProducts);
+    if (categoriesData) {
+      const filteredCategory = categoriesData?.filter(
+        (category) => category.name === currentPath.slice(1, currentPath.length)
+      );
+
+      if (filteredCategory.length > 0) {
+        console.log('filteredCategory', filteredCategory);
+        const filteredProducts = filteredCategory[0].items.reduce((acc, item) => {
+          if (item.name.split(' ')[0] === ev.target.value) {
+            return acc.concat(item);
+          }
+          return acc;
+        }, []);
+
+        setProductsToRender(filteredProducts);
+
+        // console.log('filteredProducts', filteredProducts);
+        // console.log('allFilteredProducts', allFilteredProducts);
+        // setProductsToRender([allFilteredProducts, filteredProducts]);
+        // setAllFilteredProducts([...filteredProducts, filteredProducts]);
+      } else {
+        setProductsToRender([]);
+      }
+    }
   };
+  console.log('productsToRender', productsToRender);
 
   return (
     <div className="filter-label-and-options">
--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react'; 
-import { useLocation } from 'react-router-dom'; 
+import React, { useState, useEffect, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import './CategoryPage.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
@@ -10,50 +10,51 @@ import { ProductContext } from '../../App';
 import Filter from '../../components/ui/Filters/Filter';
 import PriceFilter from '../../components/ui/Filters/PriceFilter';
 
-
 const CategoryPage = () => {
+  const categoriesData = categoryFetch();
 
-  const categoriesData  = categoryFetch();
-  
   const { productsToRender, setProductsToRender } = useContext(ProductContext);
-  const [ productTypes, setProductTypes ] = useState([])
-  const genders = ['hombre', 'mujer', 'niño', 'niña']    
+  const [productTypes, setProductTypes] = useState([]);
+  const genders = ['hombre', 'mujer', 'niño', 'niña'];
   const location = useLocation();
   const currentPath = location.pathname;
 
   useEffect(() => {
     categoriesData?.filter((category) => {
-      if (category.name === currentPath.slice(1,currentPath.length)) {
-      setProductsToRender(category.items)    
-      const itemTypes = category.items.map((product) => product.name.split(' ')[0])
-      setProductTypes([...new Set(itemTypes)])   
+      if (category.name === currentPath.slice(1, currentPath.length)) {
+        setProductsToRender(category.items);
+        const itemTypes = category.items.map((product) => product.name.split(' ')[0]);
+        setProductTypes([...new Set(itemTypes)]);
       }
-    })
-}, [categoriesData, currentPath]);
+    });
+  }, [categoriesData, currentPath]);
 
   return (
     <div>
       <Header />
-      <p className="category-title">{currentPath.slice(1,currentPath.length)}</p>
-      
+      <p className="category-title">{currentPath.slice(1, currentPath.length)}</p>
+
       <main>
-      <section className="filter-section">     
-      <Filter inputTitle="tipo de producto" inputOptions={productTypes}
-      setProductTypes={setProductTypes}
-      categoriesData={categoriesData}
-      /> 
-      <Filter inputTitle="género" inputOptions={genders}/> 
-      {/* <PriceFilter/> */}
-   {/* promo y bestseller */}
-      </section>
-      <section className="products-section">  
-      <ProductCard/>
-      </section>
+        <section className="filter-section">
+          <Filter
+            inputTitle="tipo de producto"
+            inputOptions={productTypes}
+            setProductTypes={setProductTypes}
+            categoriesData={categoriesData}
+            currentPath={currentPath}
+          />
+          <Filter inputTitle="género" inputOptions={genders} />
+          {/* <PriceFilter/> */}
+          {/* promo y bestseller */}
+        </section>
+        <section className="products-section">
+          <ProductCard />
+        </section>
       </main>
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default CategoryPage
+export default CategoryPage;
