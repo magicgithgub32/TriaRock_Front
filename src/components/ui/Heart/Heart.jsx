8569e435b612f352002ddda3871412ada3aa44f5
Rubcs <Rubcs@gmail.com>
2023-07-28 09:23:20 +0200

    amend routes again for icons

--- src/components/ui/Heart/Heart.jsx
+++ src/components/ui/Heart/Heart.jsx
@@ -25,11 +25,11 @@ const Heart = ({ product }) => {
         src={
           typeof userFavs[0] === 'string'
             ? userFavs?.includes(product._id)
-              ? '../public/red-heart.png'
-              : '../public/yellow-heart.svg'
+              ? './red-heart.png'
+              : './yellow-heart.svg'
             : userFavs?.some((favProduct) => favProduct._id === product._id)
-            ? '../public/red-heart.png'
-            : '../public/yellow-heart.svg'
+            ? './red-heart.png'
+            : './yellow-heart.svg'
         }
         className="heart"
         alt="heart"
